b9598ed5318c568d7d8ea856dd247415c53ea142
LandonLeo <landon.beert@gmail.com>
2024-05-01 11:44:38 -0500

    I do not care sue me

--- src/Blackguard/WorldGen.cs
+++ src/Blackguard/WorldGen.cs
@@ -16,13 +16,51 @@ public class WorldGen {
         Chunk ret = new(position);
 
         // TODO: Actual noise-based worldgen
+        if (rand.NextDouble() < .2) {
         for (int i = 0; i < Chunk.CHUNKSIZE; i++) {
+            for (int j = 0; j < Chunk.CHUNKSIZE; j++) {
+                if ((position.X + position.Y) % 2 == 0)
+                    ret.Tiles[i, j] = new Tile(Registry.GetDefinition<Snow>(), false);
+                else
+                    ret.Tiles[i, j] = new Tile(Registry.GetDefinition<Snow2>(), false);
+            }
+        }
+        } else if (rand.NextDouble() < .4) {
+            for (int i = 0; i < Chunk.CHUNKSIZE; i++) {
+            for (int j = 0; j < Chunk.CHUNKSIZE; j++) {
+                if ((position.X + position.Y) % 2 == 0)
+                    ret.Tiles[i, j] = new Tile(Registry.GetDefinition<Waste>(), false);
+                else
+                    ret.Tiles[i, j] = new Tile(Registry.GetDefinition<Waste2>(), false);
+            }
+        }
+        } else if (rand.NextDouble() < .6) {
+            for (int i = 0; i < Chunk.CHUNKSIZE; i++) {
+            for (int j = 0; j < Chunk.CHUNKSIZE; j++) {
+                if ((position.X + position.Y) % 2 == 0)
+                    ret.Tiles[i, j] = new Tile(Registry.GetDefinition<Grass>(), false);
+                else
+                    ret.Tiles[i, j] = new Tile(Registry.GetDefinition<Grass2>(), false);
+            }
+        }
+        } else if (rand.NextDouble() < .8) {
+            for (int i = 0; i < Chunk.CHUNKSIZE; i++) {
             for (int j = 0; j < Chunk.CHUNKSIZE; j++) {
                 if ((position.X + position.Y) % 2 == 0)
                     ret.Tiles[i, j] = new Tile(Registry.GetDefinition<Sand>(), false);
                 else
                     ret.Tiles[i, j] = new Tile(Registry.GetDefinition<Sand2>(), false);
             }
+        }
+        } else if (rand.NextDouble() < 1) {
+            for (int i = 0; i < Chunk.CHUNKSIZE; i++) {
+            for (int j = 0; j < Chunk.CHUNKSIZE; j++) {
+                if ((position.X + position.Y) % 2 == 0)
+                    ret.Tiles[i, j] = new Tile(Registry.GetDefinition<Lava>(), false);
+                else
+                    ret.Tiles[i, j] = new Tile(Registry.GetDefinition<Lava2>(), false);
+            }
+        }
         }
 
         while (rand.NextSingle() > 0.5) {
